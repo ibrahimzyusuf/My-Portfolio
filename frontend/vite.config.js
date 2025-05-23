d6e5d48d8b7c4f2c5817783f75f8d06585940428
ibrahimzyusuf <ibramhashalyusuf880@gmail.com>
2025-05-23 10:14:09 +0300

    adding preview and vite settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: [""],}
 })
