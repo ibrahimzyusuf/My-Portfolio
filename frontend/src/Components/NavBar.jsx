e9d5531880f6d91e6ea60c8682b61a42996da248
ibrahimzyusuf <ibramhashalyusuf880@gmail.com>
2025-08-21 20:15:30 +0300

    fixing the outline arround the name

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <div className="navbar w-11/12 flex items-center md:w-10/12">
                 <div className="left_navbar flex-1/4 items-center font-bold text-3xl md:text-5xl">
-                    <NavLink to={'/'} className={({isActive})=>isActive?'border-b-2 border-red-400 hover:text-lime-800':'hover:text-lime-800'}>
+                    <NavLink to={'/'} className={({isActive})=>isActive?'border-b-2 border-red-400 hover:text-lime-800 focus:outline-0':'hover:text-lime-800 focus:outline-0'}>
                     Ibrahim Yusuf
                     </NavLink>
                 </div>
